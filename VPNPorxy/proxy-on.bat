bc1ff1cf46b2d9926ed72bbb0e8fa551b1e9b755
pujin <2912009520@qq.com>
2026-07-22 14:56:42 +0800

    修改端口

--- VPNPorxy/proxy-on.bat
+++ VPNPorxy/proxy-on.bat
@@ -4,15 +4,15 @@ echo [*] 正在设置代理环境变量...
 
 setx HTTP_PROXY  "http://127.0.0.1:10888"  >nul
 setx HTTPS_PROXY "http://127.0.0.1:10888"  >nul
-setx ALL_PROXY   "socks5://127.0.0.1:10888" >nul
+setx ALL_PROXY   "socks5://127.0.0.1:10887" >nul
 
 REM 同时在当前会话生效（避免重启终端）
 set HTTP_PROXY=http://127.0.0.1:10888
 set HTTPS_PROXY=http://127.0.0.1:10888
-set ALL_PROXY=socks5://127.0.0.1:10888
+set ALL_PROXY=socks5://127.0.0.1:10887
 
 echo.
-echo [✓] 代理已开启（端口 10888）
+echo [✓] 代理已开启（端口 10888 socks 10887）
 echo     HTTP_PROXY  = %HTTP_PROXY%
 echo     HTTPS_PROXY = %HTTPS_PROXY%
 echo     ALL_PROXY   = %ALL_PROXY%
